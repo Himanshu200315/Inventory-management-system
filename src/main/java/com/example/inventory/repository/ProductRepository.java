d499292fbaa1168915ec837bb7c35480d5f6f978
Himanshu Atotiya <151147495+Himanshu200315@users.noreply.github.com>
2026-03-17 20:58:34 +0530

    Delete src directory

--- src/main/java/com/example/inventory/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.inventory.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.inventory.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
